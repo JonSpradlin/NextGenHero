a52368c371546c0999f35ca3ac0997358814c937
JonSpradlin <129249383+JonSpradlin@users.noreply.github.com>
2023-04-15 22:47:57 -0700

    starting waypoints

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -39,10 +39,34 @@ public class GameController : MonoBehaviour
             ++numberOfPlanes;
         }
 
+        if (GameObject.Find("A_Walk") == null)
+        {
+            GameObject e = Instantiate(Resources.Load("Prefabs/A_Walk") as GameObject);
+            Vector3 pos = getRandomPosition();
+            e.transform.localPosition = pos;
+        }
+        
+
+
+
         totalEggstxt.text = "Total Eggs: " + totalEggs;
         totalPlanestxt.text = "total Planes: " + maxPlanes;
 
     }
+    public Vector3 getRandomPosition()
+    {
+        CameraSupport s = Camera.main.GetComponent<CameraSupport>();
+        Assert.IsTrue(s != null);
+        Vector3 pos;
+        //               size           *  random(0-1)  * 80%  +    minimum value        +       10% of size
+        pos.x = s.GetWorldBound().size.x * Random.value * 0.8f + s.GetWorldBound().min.x + (s.GetWorldBound().size.x * 0.1f);
+        pos.y = s.GetWorldBound().size.y * Random.value * 0.8f + s.GetWorldBound().min.y + (s.GetWorldBound().size.y * 0.1f);
+
+        pos.z = 0;
+
+        return pos;
+    }
+
 
     public void EnemyDestroyed()
     {
